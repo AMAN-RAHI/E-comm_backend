02fba812ad995aaae81dc444a9311bd06452ff00
hawkeye102 <outofbox973@gmail.com>
2025-03-24 23:24:05 +0530

    added delet and get route for  myList

--- Routes/myListRoutes.js
+++ Routes/myListRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 
-import { addtomyList } from '../controllers/myListControllers.js'
+import { addtomyList,getmyListitems,deletemyListitems} from '../controllers/myListControllers.js'
 
 const MyListRouter = express.Router();
 
 MyListRouter.post('/add',protect,addtomyList)
+
+//get route
+MyListRouter.get('/:userId',getmyListitems)
+
+//delete route 
+MyListRouter.delete('/:id',protect,deletemyListitems)
 
 export default MyListRouter
--- controllers/myListControllers.js
+++ controllers/myListControllers.js
@@ -45,3 +45,60 @@ export  const addtomyList = async (req,res) => {
     }
     
 } 
+
+//get route for all items
+export const getmyListitems = async(req,res)=>{
+    try {
+        const{userId} = req.params;
+        const items =await mylistModel.find({
+            userId:userId
+        }).populate('productId')
+
+        if(!items) return res.status(404).json({
+            success:false,
+            message:"the item is not present in myList"
+        })
+
+        return res.status(200).json({
+            success:true,
+            message:'myList items',
+            items
+
+        })
+    } catch (error) {
+        console.log('server error',error),
+        res.status(500).json({
+           message:error.message || 'Internal server error',
+            success:false,
+
+        })
+    }
+}
+
+//delete the myList item controller
+export const deletemyListitems = async(req,res)=>{
+    try{
+        
+      const item = await mylistModel.findById(req.params.id)
+      if(!item) return res.status(404).json({
+        success:false,
+        message :"the item is not found in myList"
+      })
+
+      await mylistModel.findByIdAndDelete(req.params.id)
+
+      return res.status(200).json({
+        success:true,
+        message:"the item is deleted successfully",
+        item
+      })
+    }
+    catch(error){
+console.log('server error',error),
+res.status(404).json({
+    success:false,
+    message:error.message || 'Internal server error',
+
+})
+    }
+}
